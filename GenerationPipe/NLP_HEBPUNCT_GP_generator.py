a03483d88616ae6d4ad56250d78f6cd693f48d42
moshe noi <noivirt.moshe@gmail.com>
2019-02-20 20:55:10 +0200

    create a words_to_pos list for hanan

--- GenerationPipe/NLP_HEBPUNCT_GP_generator.py
+++ GenerationPipe/NLP_HEBPUNCT_GP_generator.py
@@ -186,7 +186,7 @@ readyUrlList = [
     "https://he.wikipedia.org/wiki/%D7%92%D7%91%D7%99%D7%A2_%D7%94%D7%A2%D7%95%D7%9C%D7%9D_%D7%91%D7%9B%D7%93%D7%95%D7%A8%D7%92%D7%9C",
     "https://he.wikipedia.org/wiki/%D7%91%D7%A8%D7%A6%D7%9C%D7%95%D7%A0%D7%94_(%D7%9B%D7%93%D7%95%D7%A8%D7%92%D7%9C)"]
 
-generate(readyUrlList, "2019_02_20_train_data", True)
+#generate(readyUrlList, "2019_02_20_train_data", True)
 
 # print(getStringFromDataFile("train_data"))
 
